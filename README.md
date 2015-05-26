0f3d482f2ec0db0b368bbddafe8d842d86d1b9b0
hak <hak@grokwork.com>
2015-05-26 13:49:12 +0900

    初回

---
+++ README.md
@@ -0,0 +1,31 @@
+# jquery simple slider
+
+カスタマイズが容易なタイマー付きスライダーです
+
+##機能
+
+*次へボタン
+*現在地ボタン
+*一定時間で移動
+
+##使い方
+
+こんな感じのhtmlで動作します
+
+```
+            <div id="topmain_cover">
+                <ul class="slider">
+                    <li>slide1</li>
+                    <li>slide2</li>
+                    <li>slide3</li>
+                    <li>slide4</li>
+                </ul>
+                <div class="slider_next">next</div>
+                <ul class="slider_select">
+                    <li>●</li>
+                    <li>●</li>
+                    <li>●</li>
+                    <li>●</li>
+                </ul>
+            </div>
+```
---
+++ slider.js
@@ -0,0 +1,70 @@
+$(function(){
+
+    function Slider(el) {
+        this.initialize(el);
+    }
+
+    Slider.prototype.initialize = function(el) {
+        this.$el = $(el);
+        this.$slider = this.$el.find('.slider');
+        this.$slides = this.$slider.find('li');
+        this.$len = this.$slides.length;
+        this.$next = this.$el.find('.slider_next');
+        this.$select = this.$el.find('.slider_select');
+        this.$selectBtn = this.$select.find('li');
+        this.$cullent = 0;
+        this.$slides.eq(0).show();
+        this.$selectBtn.eq(0).addClass('current');
+        this.handleEvents();
+        this.setTimer();
+        console.log(this.$el);
+    };
+
+    Slider.prototype.handleEvents = function() {
+        var self = this;
+
+        this.$next.on("click", function() {
+            self.$cullent++;
+    
+            self.changeSlide();
+            self.resetTimer();
+        });
+
+        this.$selectBtn.on("click", function() {
+            self.$cullent = self.$selectBtn.index(this);
+    
+            self.changeSlide();
+            self.resetTimer();
+        });
+
+    };
+
+    Slider.prototype.setTimer = function() {
+        var self = this;
+
+        this.timer = setInterval(function() {
+            self.$cullent++;
+            self.changeSlide();
+        }, 5000);
+    }
+
+    Slider.prototype.resetTimer = function() {
+        var self = this;
+
+        clearInterval(self.timer);
+        this.setTimer();
+    }
+
+    Slider.prototype.changeSlide = function() {
+        this.$cullent = this.$cullent % this.$len;
+
+        this.$slides.fadeOut();
+        this.$slides.eq(this.$cullent).fadeIn();
+
+        this.$selectBtn.removeClass('current');
+        this.$selectBtn.eq(this.$cullent).addClass('current');
+    }
+
+    var slider = new Slider('#slider_wrapper');
+
+});
